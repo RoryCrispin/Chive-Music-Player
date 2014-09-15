6c00750e10ba1627e44b9ec8a094d13b297638dc
Rory Crispin <me@rorycrispin.co.uk>
2014-09-15 22:37:16 +0100

    Tiles clean

--- js/tileView.js
+++ js/tileView.js
@@ -1,9 +1,8 @@
 $(".homeTile").hover(function(){
         $(this).find('.hoverOverlay').css("opacity","0.6");
-        $(this).find('.hoverOverlay i').css("opacity","1");
+
     },function(){
         $(this).find('.hoverOverlay').css("opacity","0");
-        $(this).find('.hoverOverlay i').css("opacity","0");
     }
 );
 
