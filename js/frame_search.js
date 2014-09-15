9c5f54e6cd995aa1eb47f19a723bbf18e0416efa
Rory Crispin <me@rorycrispin.co.uk>
2014-09-15 20:41:42 +0100

    Search page tiles

---
+++ js/frame_search.js
@@ -0,0 +1,1 @@
+$(".homeTile").hover(function(){     $(this).find('.hoverOverlay').css("opacity","0.6"); },function(){     $(this).find('.hoverOverlay').css("opacity","0"); } );
--- js/tileView.js
+++ js/tileView.js
@@ -1,12 +1,10 @@
-/**
- * Created by Rory on 14/09/2014.
- */
 $(".homeTile").hover(function(){
-    $(this).find('.hoverOverlay').css("opacity","0.6");
-},function(){
-    $(this).find('.hoverOverlay').css("opacity","0");
-});
+        $(this).find('.hoverOverlay').css("opacity","0.6");
+        $(this).find('.hoverOverlay i').css("opacity","1");
+    },function(){
+        $(this).find('.hoverOverlay').css("opacity","0");
+        $(this).find('.hoverOverlay i').css("opacity","0");
+    }
+);
 
-#container {
-	width:150px;
-}
+
