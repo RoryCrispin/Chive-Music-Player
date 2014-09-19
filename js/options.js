55f9b4ebe4c9aa8940df26756deea080437a85f6
Rory Crispin <me@rorycrispin.co.uk>
2014-09-19 21:24:02 +0100

    Revert "Albums work fine if all prams there"
    This reverts commit e84bf955889d5fc01de24092d94087b8919f0a84.
    
    Conflicts:
    	.idea/workspace.xml
    	js/initPage.js

--- js/options.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Rory on 21/08/2014.
- */
-var fetchMetaForYTSongs = true;
--- js/sidebar.js
+++
@@ -1,9 +0,0 @@
-$("#searchInput").keyup(function(event){
-    if(event.keyCode == 13){
-       alert("s");
-    }
-});
-function sidebar_homeClick(){
-
-    $('.container-fluid').load('html_frames/frame_home.html');
-}
