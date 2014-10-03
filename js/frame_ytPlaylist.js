8215722fd76069ef6bc44183d2b780100dfbc3e4
Rory Crispin <me@rorycrispin.co.uk>
2014-10-03 14:47:08 +0100

    fixed the bug. Called init from init

--- js/frame_ytPlaylist.js
+++ js/frame_ytPlaylist.js
@@ -2,5 +2,5 @@
  * Created by Rozz on 17/09/14.
  */
 $(function() {
-    ytGetPlaylist("PLLF9xRBewdFae_xTw1q9AUW-Vma6B5PWU");
+    ///ytGetPlaylist("PLLF9xRBewdFae_xTw1q9AUW-Vma6B5PWU");
 });
--- js/initPage.js
+++ js/initPage.js
@@ -24,6 +24,7 @@ function getFramePram(){
 
         case "ytPlaylist":
             $('.container-fluid').load('html_frames/frame_ytPlaylist.html');
+
             break;
         case "album":
             $('.container-fluid').load('html_frames/frame_albumTable.html');
